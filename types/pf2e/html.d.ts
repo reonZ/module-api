535acedde5ebc25404e41b19a48f8b360e41897d
Idle <651462+reonZ@users.noreply.github.com>
2024-02-19 03:17:23 +0100

    added more html helpers

--- types/pf2e/html.d.ts
+++ types/pf2e/html.d.ts
@@ -1,6 +1,34 @@
 /**
- * @param {unknown} parent
+ * @param {HTMLElement} parent
  * @param {string} selectors
  * @returns {HTMLElement|null}
  */
-export function htmlQuery(parent: unknown, selectors: string): HTMLElement | null;
+export function htmlQuery(parent: HTMLElement, selectors: string): HTMLElement | null;
+/**
+ * @param {HTMLElement} parent
+ * @param {string} selectors
+ * @returns {NodeListOf<HTMLElement>}
+ */
+export function htmlQueryAll(parent: HTMLElement, selectors: string): NodeListOf<HTMLElement>;
+/**
+ * @param {string} nodeName
+ * @param {{
+ *   classes?: string[];
+ *   dataset?: Record<string, string | number | boolean | null | undefined>;
+ *   children?: (HTMLElement | string)[];
+ *   innerHTML?: string;
+ * }}
+ * @returns {HTMLElement}
+ */
+export function createHTMLElement(nodeName: string, { classes, dataset, children, innerHTML }?: {
+    classes?: string[];
+    dataset?: Record<string, string | number | boolean | null | undefined>;
+    children?: (HTMLElement | string)[];
+    innerHTML?: string;
+}): HTMLElement;
+/**
+ * @param {HTMLElement} child
+ * @param {string} selectors
+ * @returns {HTMLElement|null}
+ */
+export function htmlClosest(child: HTMLElement, selectors: string): HTMLElement | null;
