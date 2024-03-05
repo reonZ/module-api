cb7e31455f228010c053a049211d820931bf7f10
Idle <651462+reonZ@users.noreply.github.com>
2024-03-05 17:19:14 +0100

    add registerSetting

--- foundry/settings.js
+++ foundry/settings.js
@@ -1,3 +1,6 @@
+import { arrayToObject } from "../utils";
+import { MODULE } from "./module";
+
 /**
  * @param {object} setting
  * @returns {boolean}
@@ -12,4 +15,33 @@ export function isWorldSetting(setting) {
  */
 export function isClientSetting(setting) {
 	return !setting.restricted || setting.scope === "client";
+}
+
+/**
+ * @template {number | boolean | string} T
+ * @param { {key: string, type: new (...args: unknkown[]) => T, default: T, [k: string]: unknown }} options
+ */
+export function registerSetting(options) {
+	options.key ??= options.name;
+
+	if (Array.isArray(options.choices)) {
+		options.choices = arrayToObject(options.choices, (choice) =>
+			settingPath(options.key, "choices", choice),
+		);
+	}
+
+	options.name = settingPath(options.key, "name");
+	options.hint = settingPath(options.key, "hint");
+	options.scope ??= "world";
+	options.config ??= true;
+
+	game.settings.register(MODULE.id, options.key, options);
+}
+
+/**
+ * @param {string[]} path
+ * @returns {string}
+ */
+export function settingPath(...path) {
+	return `${MODULE.id}.settings.${path.join(".")}`;
 }
--- types/foundry/settings.d.ts
+++ types/foundry/settings.d.ts
@@ -8,3 +8,18 @@ export function isWorldSetting(setting: object): boolean;
  * @returns {boolean}
  */
 export function isClientSetting(setting: object): boolean;
+/**
+ * @template {number | boolean | string} T
+ * @param { {key: string, type: new (...args: unknkown[]) => T, default: T, [k: string]: unknown }} options
+ */
+export function registerSetting<T extends string | number | boolean>(options: {
+    [k: string]: unknown;
+    key: string;
+    type: new (...args: unknkown[]) => T;
+    default: T;
+}): void;
+/**
+ * @param {string[]} path
+ * @returns {string}
+ */
+export function settingPath(...path: string[]): string;
