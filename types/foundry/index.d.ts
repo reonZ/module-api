a2b2147f85566659bfd390cf5418720ba9bc9315
Idle <651462+reonZ@users.noreply.github.com>
2024-03-05 17:12:25 +0100

    indexes

---
+++ types/foundry/index.d.ts
@@ -0,0 +1,4 @@
+export * from "./libwrapper";
+export * from "./localize";
+export * from "./module";
+export * from "./settings";
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,3 @@
+export * from "./foundry";
+export * from "./utils";
 export * as R from "remeda";
