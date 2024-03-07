7a93b933dca46361aecac5c5e4976e81caf863ea
Idle <651462+reonZ@users.noreply.github.com>
2024-03-07 19:06:47 +0100

    add render

--- foundry/app.js
+++ foundry/app.js
@@ -8,3 +8,13 @@ import { MODULE } from "./module";
 export function templatePath(...path) {
 	return `modules/${MODULE.id}/templates/${joinStr("/", path)}.hbs`;
 }
+
+/**
+ * @param  {(string|object)[]} args
+ * @returns {Promise<string>}
+ */
+export function render(...args) {
+	const data = typeof args.at(-1) === "object" ? args.splice(-1)[0] : {};
+	const path = templatePath(...args);
+	return renderTemplate(path, data);
+}
--- types/foundry/app.d.ts
+++ types/foundry/app.d.ts
@@ -3,3 +3,8 @@
  * @returns {string}
  */
 export function templatePath(...path: string[]): string;
+/**
+ * @param  {(string|object)[]} args
+ * @returns {Promise<string>}
+ */
+export function render(...args: (string | object)[]): Promise<string>;
